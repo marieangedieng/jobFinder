715efee3289a90a46bcf8421ffd3e4c66edaf8b9
Marie-Ange DIENG <marieangedieng@gmail.com>
2024-06-18 04:00:23 +0000

    Menu for small screens Mod

--- resources/views/frontend/layouts/header.blade.php
+++ resources/views/frontend/layouts/header.blade.php
@@ -64,7 +64,7 @@
     <div class="mobile-header-wrapper-inner">
       <div class="mobile-header-content-area">
         <div class="perfect-scroll">
-          <div class="mobile-header-logo mobile-header-border mb-30">
+          <div class="mobile-header-logo mobile-header-border mb-20">
                   <a class='d-flex' href='{{ url('/') }}' >
                       <img alt="jobFinder" src="{{ asset('frontend/assets/imgs/page/homepage1/headerLogoDark.png')}}" >
                   </a>
