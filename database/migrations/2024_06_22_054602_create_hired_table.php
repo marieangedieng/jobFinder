6c7326cc0621e654027090f163f577e242f26074
Marie-Ange DIENG <marieangedieng@gmail.com>
2024-06-22 05:49:18 +0000

    Add Hired Table

---
+++ database/migrations/2024_06_22_054602_create_hired_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHiredTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('hired', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('candidate_id');
+            $table->unsignedBigInteger('job_id');
+            $table->timestamps();
+
+            $table->foreign('candidate_id')->references('id')->on('candidates')->onDelete('cascade');
+            $table->foreign('job_id')->references('id')->on('jobs')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('hired');
+    }
+}
+
